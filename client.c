95731ff6773c72b451f348b80845282d5cace56b
Florian Hensel <florinator543@gmail.com>
2026-01-17 17:21:40 +0100

    Add initial client and server implementation comments

---
+++ client.c
@@ -0,0 +1,3 @@
+// takes PID and message
+// go through the string bit by bit
+// with PID send Signal for 1 or 0 to server
---
+++ server.c
@@ -0,0 +1,4 @@
+// with start prints its PID
+// waits for a signal
+// check what signal we got and add it
+// if we got all 8bits we write and reset for the next set of 8 till we got everything
