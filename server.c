3393b3e9a687a10e6ae8ed3bfaa5e728b7324eeb
Hyperdrax <florinator543@gmail.com>
2026-01-18 14:41:15 +0100

    Added signal handling in server implementation

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: flhensel <flhensel@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 12:28:16 by florianh          #+#    #+#             */
-/*   Updated: 2026/01/18 14:01:37 by flhensel         ###   ########.fr       */
+/*   Updated: 2026/01/18 14:31:03 by flhensel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,30 @@
 
 #include "minitalk.h"
 
+static void handle_signal(int sig)
+{
+    static int bit_count = 0;
+    static char current_char = 0;
+
+    if (sig == SIGUSR1)
+        current_char |= (1 << bit_count);
+    bit_count++;
+    if (bit_count == 8)
+    {
+        write(1, &current_char, 1);
+        bit_count = 0;
+        current_char = 0;
+    }
+}
+
 int main(void)
 {
     // Implementation of server logic
     pid_t pid = getpid();
-    ft_printf("Server PID: %d\n", pid);
 
+    ft_printf("Server PID: %d\n", pid);
+    signal(SIGUSR1, handle_signal);
+    signal(SIGUSR2, handle_signal);
     while (1)
     {
         pause(); // Wait for signals
