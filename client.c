104f4967618be9cdd63d8ef7649863fc9848ccf6
Florian Hensel <florinator543@gmail.com>
2026-01-19 13:11:54 +0100

    fixed Norm

--- client.c
+++ client.c
@@ -6,15 +6,15 @@
 /*   By: flhensel <flhensel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 12:28:23 by flhensel          #+#    #+#             */
-/*   Updated: 2026/01/19 12:30:40 by flhensel         ###   ########.fr       */
+/*   Updated: 2026/01/19 13:11:32 by flhensel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static int ft_atoi(const char *str)
+static int	ft_atoi(const char *str)
 {
-	int num;
+	int	num;
 
 	num = 0;
 	while (*str >= '0' && *str <= '9')
@@ -25,9 +25,9 @@ static int ft_atoi(const char *str)
 	return (num);
 }
 
-static void send_byte(pid_t pid, unsigned char byte)
+static void	send_byte(pid_t pid, unsigned char byte)
 {
-	int j;
+	int	j;
 
 	j = 7;
 	while (j >= 0)
@@ -41,23 +41,22 @@ static void send_byte(pid_t pid, unsigned char byte)
 	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	pid_t pid;
-	int i;
-	
+	pid_t	pid;
+	int		i;
+
 	if (argc != 3)
 	{
-		write(2,"Usage: ./client <pid> <message>\n", 32);
-		return(1);
+		write(2, "Usage: ./client <pid> <message>\n", 32);
+		return (1);
 	}
 	pid = ft_atoi(argv[1]);
 	if (pid <= 0)
 	{
-		write(2,"Invalid PID\n",12);
-		return(0);
+		write(2, "Invalid PID\n", 12);
+		return (0);
 	}
-
 	i = 0;
 	while (argv[2][i])
 	{
@@ -65,5 +64,5 @@ int main(int argc, char **argv)
 		i++;
 	}
 	send_byte(pid, '\n');
-	return(0);
+	return (0);
 }
--- server.c
+++ server.c
@@ -6,37 +6,37 @@
 /*   By: flhensel <flhensel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 12:28:16 by florianh          #+#    #+#             */
-/*   Updated: 2026/01/19 12:31:24 by flhensel         ###   ########.fr       */
+/*   Updated: 2026/01/19 13:09:47 by flhensel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static void handle_signal(int sig)
+static	void	handle_signal(int sig)
 {
-    static int bit_count = 0;
-    static char current_char = 0;
+	static int	bit_count = 0;
+	static char	current_char = 0;
 
-    if (sig == SIGUSR2)
-        current_char |= (1 << (7 - bit_count));
-    bit_count++;
-    if (bit_count == 8)
-    {
-        write(1, &current_char, 1);
-        bit_count = 0;
-        current_char = 0;
-    }
+	if (sig == SIGUSR2)
+		current_char |= (1 << (7 - bit_count));
+	bit_count++;
+	if (bit_count == 8)
+	{
+		write(1, &current_char, 1);
+		bit_count = 0;
+		current_char = 0;
+	}
 }
 
-int main(void)
+int	main(void)
 {
-    pid_t pid = getpid();
+	pid_t	pid;
 
-    ft_printf("Server PID: %d\n", pid);
-    signal(SIGUSR1, handle_signal);
-    signal(SIGUSR2, handle_signal);
-    while (1)
-    {
-        pause();
-    return 0;
+	pid = getpid();
+	ft_printf("Server PID: %d\n", pid);
+	signal(SIGUSR1, handle_signal);
+	signal(SIGUSR2, handle_signal);
+	while (1)
+		pause();
+	return (0);
 }
